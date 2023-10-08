dfae550f4cd35a46163c78165a7ea92ff2312e09
kirill <kirillandreevickozlov@yandex.ru>
2023-10-08 17:48:45 +0300

    task2.2.1

--- task2.2.1.c
+++ task2.2.1.c
@@ -2,12 +2,14 @@
 
 int main() {
     double x,sum,a;
+    printf("task2.2.1\n");
+    printf("Enter x and odds: ");
     scanf("%lf",&x);
     sum = 0;
     while (scanf("%lf",&a) != EOF) {
         sum *= x;
         sum += a;
     }
-    printf("%lf\n",sum);
+    printf("Value of f(x) in x = %lf\n",sum);
     return 0;
-} 
+}
