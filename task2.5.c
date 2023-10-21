0e0ba0a91ebc973380c7fbe0ebbd7af682334dae
kirill <kirillandreevickozlov@yandex.ru>
2023-10-21 13:39:02 +0300

    task2.5

--- task2.5.c
+++ task2.5.c
@@ -108,7 +108,9 @@ int main() {
         }
 
 
-        // list after deletes
+
+
+        // list after deletes //
         printf("\n List after  deletes: ");
         p = lst;
         while (p != NULL){
